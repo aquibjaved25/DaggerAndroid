0173b27ae31f7b7d9c97e9bbe9c1ca4a9709b744
aquibjaved25 <aquib.javed25@gmail.com>
2023-03-20 15:34:33 +0530

    added singleton

--- app/src/main/java/com/myapplication/di/AppModule.kt
+++ app/src/main/java/com/myapplication/di/AppModule.kt
@@ -8,19 +8,21 @@ import com.bumptech.glide.request.RequestOptions
 import com.myapplication.R
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
 
     companion object {
+        @Singleton
         @Provides
         fun provideRequestOptions(): RequestOptions = RequestOptions()
             .placeholder(R.drawable.white_background)
             .error(R.drawable.white_background)
-
+        @Singleton
         @Provides
         fun provideGlideInstance(application: Application,requestOptions: RequestOptions) = Glide.with(application).setDefaultRequestOptions(requestOptions)
-
+        @Singleton
         @Provides
         fun provideAppDrawable(application: Application):Drawable =
             ContextCompat.getDrawable(application,R.drawable.logo)!!
